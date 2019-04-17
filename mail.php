6092ee44a9a6a86d319163c9e2b60ec7af4af6bd
Viacheslav Pastushenko <slavpas@gmail.com>
2019-04-17 23:41:58 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,16 +1,25 @@
 <?php
 if (isset($_POST['email'])) {
+    $json = array();
+
     $to = "hello@brandux.agency";
     $from = "hello@brandux.agency";
     $subject = "Новая заявка с сайта ". $_SERVER['HTTP_REFERER'];
-    $message = "Email: " . $_POST['email'];
+
+    $message = "Новая заявка\n";
+
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        $message .= "\nEmail: " . $_POST['email'];
+    }
 
     if (isset($_POST['firstname']) && !empty($_POST['firstname'])) {
         $message .= "\nИмя: " . $_POST['firstname'];
     }
+
     if (isset($_POST['phone']) && !empty($_POST['phone'])) {
         $message .= "\nТелефон: " . $_POST['phone'];
     }
+
     if (isset($_POST['message']) && !empty($_POST['message'])) {
         $message .= "\nКомметарий: " . $_POST['message'];
     }
@@ -21,27 +30,35 @@ if (isset($_POST['email'])) {
     $headers .= "From: " . $from . "\r\n";
     $headers .= "Reply-To: " . $from . "\r\n";
     $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
-    $message = "Content-Type: multipart/mixed; boundary=\"$boundary\"
-                --$boundary
-                Content-Type: text/plain; charset=\"utf-8\"
-                Content-Transfer-Encoding: 7bit
-                $message";
+    $message="
+Content-Type: multipart/mixed; boundary=\"$boundary\"
+ 
+--$boundary
+Content-Type: text/plain; charset=\"utf-8\"
+Content-Transfer-Encoding: 7bit
+ 
+$message";
+     if(is_uploaded_file($_FILES['file']['tmp_name'])) {
+         $attachment = chunk_split(base64_encode(file_get_contents($_FILES['file']['tmp_name'])));
+         $filename = $_FILES['file']['name'];
+         $filetype = $_FILES['file']['type'];
+         $filesize = $_FILES['file']['size'];
+         $message.="
+ 
+--$boundary
+Content-Type: \"$filetype\"; name=\"$filename\"
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename=\"$filename\"
+ 
+$attachment";
+     }
+   $message.="
+--$boundary--";
 
-    if(is_uploaded_file($_FILES['file']['tmp_name'])) {
-        $attachment = chunk_split(base64_encode(file_get_contents($_FILES['file']['tmp_name'])));
-        $filename = $_FILES['file']['name'];
-        $filetype = $_FILES['file']['type'];
-        $filesize = $_FILES['file']['size'];
-        $message.="--$boundary
-        Content-Type: \"$filetype\"; name=\"$filename\"
-        Content-Transfer-Encoding: base64
-        Content-Disposition: attachment; filename=\"$filename\"
-        $attachment";
-    }
-    
-    $message.="--$boundary--";
+$json['success'] = true;
     
     mail($to, $subject, $message, $headers);
-    return true;
+    echo json_encode($json);
+    die();
 }
 ?>
